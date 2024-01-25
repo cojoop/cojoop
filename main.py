bcff2922d588138635146d5fcee91512ac3bd293
Seunghyun Kim <tmdgus8779@gmail.com>
2024-01-26 02:02:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ MAX_POST = 5
 
 markdown_text = """
 
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FimseunghyunK&count_bg=%23CEB0BB&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
 ### Hi there 👋, my name is Seunghyun <br>
 I wanna be a developer who makes <b>code deliciously</b>.
 
@@ -46,10 +47,6 @@ for idx, feed in enumerate(RSS_FEED['entries']):
         feed_date = feed['published_parsed']
         markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
 
-"""
-[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FimseunghyunK&count_bg=%23CEB0BB&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
-"""
-        
 f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
 f.close()

e123e740748cd6caed4780959ed3b9725a756144
Seunghyun Kim <tmdgus8779@gmail.com>
2024-01-28 02:19:10 +0900

    update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ I wanna be a developer who makes <b>code deliciously</b>.
 - 📩 How to reach me: tmdgus8779@gmail.com
 <br>
 
-**🛠️ My Stack**  
+**🛠️ Tech Stack**  
 <div>
   <img src="https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=Flask&logoColor=white">
   <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=white">

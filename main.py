a23b7e3b290c21f002a98514143f33a3e290fa50
Seunghyun Kim <tmdgus8779@gmail.com>
2024-03-07 12:45:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ markdown_text = """
 </div>
 &nbsp;
 <div align="center">
-  <img src="https://img.shields.io/badge/c-A8B9CC?style=for-the-badge&logo=c&logoColor=white">
   <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=Python&logoColor=white">
   <img src="https://img.shields.io/badge/Java-007396?style=for-the-badge&logo=openjdk&logoColor=white">
 </div>

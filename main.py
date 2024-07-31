5f44c9587a1afe4d6a3fdcfd816a0ba2b645a8cd
Seunghyun Kim <tmdgus8779@gmail.com>
2024-07-31 12:26:32 +0900

    feat: update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ markdown_text = """
 
 ## I'm a Department of Computer Science student at Korea National Open University
 
-- 🔥 I’m currently learning Servlet.
-- 🌱 I'm looking to study Spring soon.
+- 🔥 I’m currently learning Spring.
+- 🌱 I'm looking to study SpringBoot soon.
 - 👥 I'm looking to collaborate with anyone on any project.
 - 💬 Ask me anything.
 - ⭐️ Fun fact: I watch all the major European football matches live.
